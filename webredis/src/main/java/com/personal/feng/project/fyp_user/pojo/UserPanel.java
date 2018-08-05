4ef089bde317e8123f99741a5c9138c41622dfc3
YunpengFeng <1565370422@qq.com>
2018-08-05 10:24:09 +0800

    user的panel方法

---
+++ webredis/src/main/java/com/personal/feng/project/fyp_user/pojo/UserPanel.java
@@ -0,0 +1,14 @@
+package com.personal.feng.project.fyp_user.pojo;
+
+public class UserPanel {
+
+    private User user ;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
